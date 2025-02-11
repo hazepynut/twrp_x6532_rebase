c71d2a5d53d76444c6601deddcd2048bf05d0b63
hazepynut <naden.hey@gmail.com>
2025-02-11 00:57:13 -0800

    Update and rename omni_Infinix-X6532.mk to twrp_X6532.mk

--- twrp_X6532.mk
+++ twrp_X6532.mk
@@ -8,21 +8,22 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6532 device
-$(call inherit-product, device/infinix/Infinix-X6532/device.mk)
+$(call inherit-product, device/infinix/X6532/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6532
-PRODUCT_NAME := omni_Infinix-X6532
+PRODUCT_NAME := twrp_X6532
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6532
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_x6532_v632-user 12 SP1A.210812.016 722914 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6532-OP/Infinix-X6532:12/SP1A.210812.016/241017V2228:user/release-keys
