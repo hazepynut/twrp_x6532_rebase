eab4c0438d6439d2ec4a4a1b359fa5eff5f97f92
hazepynut <naden.hey@gmail.com>
2025-02-11 00:38:20 -0800

    twrp tree

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for Infinix Infinix X6532 (Infinix-X6532)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Infinix-X6532)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
